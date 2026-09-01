6a6a60bdf93acf8d53754ed0e75ae70a652a8398
NK Studio <NK-Studio@users.noreply.github.com>
2026-09-02 03:35:06 +0900

    [add] unity 카본 추가

---
+++ Unity-Carbon/_next/static/chunks/pages/embed-0deb65d918e11970.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[288],{9079:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/embed",function(){return u(3867)}])}},function(n){n.O(0,[774,834,126,369,152,843,94,363,867,888,179],(function(){return _=9079,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
